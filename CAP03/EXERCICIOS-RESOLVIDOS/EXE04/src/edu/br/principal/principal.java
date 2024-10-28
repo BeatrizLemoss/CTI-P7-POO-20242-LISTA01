47e18177ed61b248427da4cf6819c7c8e74dea6c
Beatriz Lemos <beatriz.lemos07@aluno.ifce.edu.br>
2024-10-28 14:50:54 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE04/src/edu/br/principal/principal.java
@@ -0,0 +1,13 @@
+package edu.br.principal;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		 double salario = 1000.0; 
+	        double novo_salario = salario + (salario * 0.25);
+	        System.out.println("O novo salário é " + novo_salario);
+
+	}
+
+}
