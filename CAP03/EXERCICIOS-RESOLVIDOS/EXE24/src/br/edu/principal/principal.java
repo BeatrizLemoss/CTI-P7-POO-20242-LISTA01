99a9556ad3dd1baeb470e1bd65de75317167f218
Beatriz Lemos <beatriz.lemos07@aluno.ifce.edu.br>
2024-11-04 14:33:56 -0300

    principal.java

--- CAP03/EXERCICIOS-RESOLVIDOS/EXE24/src/br/edu/principal/principal.java
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE24/src/br/edu/principal/principal.java
@@ -3,7 +3,22 @@ package br.edu.principal;
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	double hora = 2.30; 
+        int h = (int) hora; 
+        double minutos = (hora - h) * 100; // Cálculo dos minutos
+
+        // Ajuste para garantir que minutos não sejam maiores que 59
+        if (minutos >= 60) {
+            h += (int) minutos / 60; 
+            minutos = minutos % 60; 
+        }
+
+        // Conversão total em minutos
+        int conversao = (h * 60) + (int) minutos;  
+
+        // Saída formatada
+        System.out.println(h + " horas e " + (int) minutos + " minutos equivalem a " + conversao + " minutos.");
+
 
 	}
 
