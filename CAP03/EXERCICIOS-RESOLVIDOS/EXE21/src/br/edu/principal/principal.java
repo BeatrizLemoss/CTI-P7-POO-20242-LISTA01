006e80a9952c7f6affb89cdbe57772b8e3c47678
Beatriz Lemos <beatriz.lemos07@aluno.ifce.edu.br>
2024-11-04 14:36:19 -0300

    principal.java

--- CAP03/EXERCICIOS-RESOLVIDOS/EXE21/src/br/edu/principal/principal.java
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE21/src/br/edu/principal/principal.java
@@ -3,7 +3,19 @@ package br.edu.principal;
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double Y;
+		double X = 3.0;
+		double Z = 4.0;
+		
+		Y = Z * Z - X * X;
+		if (Y >= 0) {
+		Y = Math.sqrt(Y);
+		}else {
+            System.out.println("Não é possível calcular a raiz quadrada de um número negativo.");
+            Y = 0; 
+        }		
+		
+		System.out.print("A distância que a escada tem que ficar da parede é: "  + Y);		
 
 	}
 
