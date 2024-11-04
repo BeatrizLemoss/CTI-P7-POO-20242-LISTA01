83b138fae59dc1b1a4ad516245e563ab46d00ac6
Beatriz Lemos <beatriz.lemos07@aluno.ifce.edu.br>
2024-11-04 14:34:30 -0300

    principal.java

--- CAP03/EXERCICIOS-RESOLVIDOS/EXE23/src/br/edu/principal/principal.java
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE23/src/br/edu/principal/principal.java
@@ -3,7 +3,12 @@ package br.edu.principal;
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	double num = 5.75;
+		int inteiro = (int) num;
+		double fracionario = num - inteiro;
+		System.out.println("A parte inteira é: " + inteiro);
+		System.out.println("A parte fracionária é: " + fracionario);
+		System.out.println("O arredondamento do número é: " + Math.round(num));
 
 	}
 
