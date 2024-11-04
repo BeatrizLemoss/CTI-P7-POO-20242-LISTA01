405effd2bbeccc76f7f63bda57fa56f43e8f037d
Beatriz Lemos <beatriz.lemos07@aluno.ifce.edu.br>
2024-11-04 14:38:46 -0300

    principal.java

--- CAP03/EXERCICIOS-RESOLVIDOS/EXE18/src/br/edu/principal/principal.java
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE18/src/br/edu/principal/principal.java
@@ -6,15 +6,15 @@ public class principal {
 		
 			
 		 
-	        double peso_saco = 5.0; 
-	        double racao_gato1 = 200 / 1000; 
-	        double racao_gato2 = 250 / 1000; 
-	        
-
-	        double total_final = peso_saco - 5 * (racao_gato1 + racao_gato2);
-	        
-	   
-	        System.out.printf("A quantidade de ração restante é: " + total_final + " kg");
+			        double peso_saco = 5.0; 
+			        double racao_gato1 = 200.0/1000; 
+			        double racao_gato2 = 250.0/1000;
+			        
+			        double racaoTotal = 5 * (racao_gato1+ racao_gato2);
+			        double total_final = peso_saco - racaoTotal;
+			        
+			   
+			        System.out.printf("A quantidade de ração restante é: " + total_final + " kg");
 	        
 
 	}
