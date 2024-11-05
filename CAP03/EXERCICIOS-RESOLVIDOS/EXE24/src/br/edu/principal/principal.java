4622ba239414f5d9817342343d7294d0bc4adb5a
Beatriz Lemos <beatriz.lemos07@aluno.ifce.edu.br>
2024-11-05 12:40:26 -0300

    principal.java

--- CAP03/EXERCICIOS-RESOLVIDOS/EXE24/src/br/edu/principal/principal.java
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE24/src/br/edu/principal/principal.java
@@ -5,18 +5,16 @@ public class principal {
 	public static void main(String[] args) {
 	double hora = 2.30; 
         int h = (int) hora; 
-        double minutos = (hora - h) * 100; // Cálculo dos minutos
+        double minutos = (hora - h) * 100; 
 
-        // Ajuste para garantir que minutos não sejam maiores que 59
+ 
         if (minutos >= 60) {
             h += (int) minutos / 60; 
             minutos = minutos % 60; 
         }
 
-        // Conversão total em minutos
         int conversao = (h * 60) + (int) minutos;  
 
-        // Saída formatada
         System.out.println(h + " horas e " + (int) minutos + " minutos equivalem a " + conversao + " minutos.");
 
 
